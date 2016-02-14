06063ea564190a07668aaecfd9667c55d400b981
Fredrik Heimsæter <freheims@gmail.com>
2016-02-14 21:54:08 +0100

    More Java database stuff

--- resources/Semantics/src/database/DbCredentials.java
+++ resources/Semantics/src/database/DbCredentials.java
@@ -4,4 +4,24 @@ package database;
  * Created by fredrik on 2/14/16.
  */
 public class DbCredentials {
+    private static String HOST ="";
+    private static String USER ="";
+    private static String PASSWORD ="";
+    private static String DATABASE ="";
+
+    public static String getHOST() {
+        return HOST;
+    }
+
+    public static String getUSER() {
+        return USER;
+    }
+
+    public static String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public static String getDATABASE() {
+        return DATABASE;
+    }
 }
