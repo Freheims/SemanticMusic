d0f1e0dc0e75afb70b3dc12b0388d8d29ae9e163
Fredrik Heimsæter <freheims@gmail.com>
2016-02-14 18:25:00 +0100

    Added SQL for database creation

---
+++ resources/database.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS artist (
+    id char(22) NOT NULL,
+    name varchar(60) NOT NULL,
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS album (
+    id char(22) NOT NULL,
+    name varchar(120) NOT NULL,
+    type varchar(20),
+    image varchar(70),
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS track (
+    id char(22) NOT NULL,
+    name varchar(60) NOT NULL,
+    annotated boolean DEFAULT FALSE,
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS genre (
+    name varchar(30) NOT NULL,
+    PRIMARY KEY(name)
+    );
+
+CREATE TABLE IF NOT EXISTS trackGenre (
+    id int(10) NOT NULL AUTO_INCREMENT,
+    track_id char(22) NOT NULL,
+    genre char(22) NOT NULL,
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS trackArtist (
+    id int(10) NOT NULL AUTO_INCREMENT,
+    track_id char(22) NOT NULL,
+    artist_id char(22) NOT NULL,
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS trackAlbum (
+    id int(10) NOT NULL AUTO_INCREMENT,
+    track_id char(22) NOT NULL,
+    album_id char(22) NOT NULL,
+    PRIMARY KEY(id)
+    );
+
