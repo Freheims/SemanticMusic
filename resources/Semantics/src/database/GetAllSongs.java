e45d188bbb482a3a5c0b1aeb44d95c66e9f26795
Fredrik Heimsæter <freheims@gmail.com>
2016-02-14 22:11:18 +0100

    More shit

--- resources/Semantics/src/database/GetAllSongs.java
+++ resources/Semantics/src/database/GetAllSongs.java
@@ -26,7 +26,7 @@ public class GetAllSongs {
             conn = DriverManager.getConnection("jdb:mysql://" + host + "/" + db + "?" + "user=" + user + "&password=" + psw );
 
             statement = conn.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM spotify.artist");
+            resultSet = statement.executeQuery("SELECT * FROM spotify.artist WHERE annotated == FALSE");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,6 +36,41 @@ public class GetAllSongs {
         return  songs;
     }
 
+    private Song createSong(ResultSet resultSet) throws Exception{
+        String id = resultSet.getString("id");
+        String name = resultSet.getString("name");
+        String artistName = getArtist(id);
+        String albumName = getAlbum(id);
+        String[] genre = getGenre(id);
+        Song song = new Song(id, name, artistName, albumName, genre);
+        return song;
+    }
 
+    /**
+     * Method for getting the name of the album
+     * @param songID The spotify-id for the song
+     * @return The name of the album
+     */
+    private String getAlbum(String songID){
+        return null; //TODO implement
+    }
+
+    /**
+     * Method for getting the name of the artist
+     * @param songID The spotify-id for the song
+     * @return The name of the artist
+     */
+    private String getArtist(String songID){
+        return null; //TODO implement
+    }
+
+    /**
+     * Method for getting the genres for a song
+     * @param songID The spotify-id for the song
+     * @return An array of genres
+     */
+    private String[] getGenre(String songID){
+        return null; //TODO implement
+    }
 
 }
