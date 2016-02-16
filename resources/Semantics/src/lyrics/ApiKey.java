0bfb0b2ca50d35c47b2b046d0754569ef44e235f
Fredrik Heimsæter <freheims@gmail.com>
2016-02-16 12:58:01 +0100

    ApiKey

---
+++ resources/Semantics/src/lyrics/ApiKey.java
@@ -0,0 +1,12 @@
+package lyrics;
+
+/**
+ * Created by fredrik on 2/16/16.
+ */
+public class ApiKey {
+    private static final String API_KEY="";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
