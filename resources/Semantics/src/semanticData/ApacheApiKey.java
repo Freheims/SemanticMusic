1bf211779eced834a539e492a005293b77090940
Fredrik Heimsæter <freheims@gmail.com>
2016-02-16 13:36:21 +0100

    Changes

---
+++ resources/Semantics/src/semanticData/ApacheApiKey.java
@@ -0,0 +1,12 @@
+package semanticData;
+
+/**
+ * Created by fredrik on 2/16/16.
+ */
+public class ApacheApiKey {
+    private static String API_KEY = "";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
---
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -0,0 +1,14 @@
+package semanticData;
+
+import com.alchemyapi.api.AlchemyApi;
+import com.alchemyapi.api.AlchemyApiConfiguration;
+
+/**
+ * Created by fredrik on 2/16/16.
+ */
+public class GetSemantics {
+
+    private static String API_KEY =
+
+    AlchemyApi alchemyApi = new AlchemyApi(new AlchemyApiConfiguration());
+}
