f391020313cd20d245c5b687b5fed72800f09d89
Fredrik Heimsæter <freheims@gmail.com>
2016-02-17 11:56:22 +0100

    Trying to get semantic data. Not working yet, because someone decided that the method getting an XML-object should be of jsonup-Document type!!

--- resources/Semantics/src/semanticData/GetSemantics.java
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -2,13 +2,41 @@ package semanticData;
 
 import com.alchemyapi.api.AlchemyApi;
 import com.alchemyapi.api.AlchemyApiConfiguration;
+import model.Song;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
 
 /**
  * Created by fredrik on 2/16/16.
  */
 public class GetSemantics {
 
-    private static String API_KEY =
+    private static String API_KEY = ApacheApiKey.getApiKey();
 
-    AlchemyApi alchemyApi = new AlchemyApi(new AlchemyApiConfiguration());
+    private static AlchemyApi alchemyApi = new AlchemyApi(new AlchemyApiConfiguration(API_KEY));
+
+    public static void getConcepts(Song song){
+        if(song.getLyrics() == "") return;
+        ArrayList<String> conceptsList =  new ArrayList<>();
+        Document conceptObject = alchemyApi.textGetRankedConcepts(song.getLyrics());
+        //System.out.println(conceptObject);
+        Elements concepts = conceptObject.getElementsByTag("<concepts>");
+        System.out.println(concepts == null);
+        for(Element con : concepts){
+            System.out.println("con: " + con == null);
+            String name = con.getElementById("text").ownText();
+            double relevance = Double.parseDouble(con.getElementById("relevance").ownText());
+            if(relevance > 0.4){
+                conceptsList.add(name);
+            }
+        }
+        song.setConcepts(conceptsList);
+
+    }
+
+
 }
