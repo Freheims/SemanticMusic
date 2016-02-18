e1fb9148f1a2e1e29bcb3c5de9c02841f180c33d
Fredrik Heimsæter <freheims@gmail.com>
2016-02-18 20:29:34 +0100

    Tried some XML. Not working whatsoever

--- resources/Semantics/src/semanticData/GetSemantics.java
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -3,14 +3,22 @@ package semanticData;
 import com.alchemyapi.api.AlchemyApi;
 import com.alchemyapi.api.AlchemyApiConfiguration;
 import model.Song;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpression;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
+ * Class for getting sematic data from Alchemy
  * Created by fredrik on 2/16/16.
  */
 public class GetSemantics {
@@ -21,20 +29,34 @@ public class GetSemantics {
 
     public static void getConcepts(Song song){
         if(song.getLyrics() == "") return;
-        ArrayList<String> conceptsList =  new ArrayList<>();
-        Document conceptObject = alchemyApi.textGetRankedConcepts(song.getLyrics());
-        //System.out.println(conceptObject);
-        Elements concepts = conceptObject.getElementsByTag("<concepts>");
-        System.out.println(concepts == null);
-        for(Element con : concepts){
-            System.out.println("con: " + con == null);
-            String name = con.getElementById("text").ownText();
-            double relevance = Double.parseDouble(con.getElementById("relevance").ownText());
-            if(relevance > 0.4){
-                conceptsList.add(name);
+        ArrayList<String> conceptsList = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder;
+            Document doc = null;
+            XPathExpression expr = null;
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(String.valueOf(alchemyApi.textGetRankedConcepts(song.getLyrics())));
+
+            XPathFactory xFactory = XPathFactory.newInstance();
+
+            XPath xPath = xFactory.newXPath();
+
+            expr = xPath.compile("//concepts");
+            Object result = expr.evaluate(doc, XPathConstants.NODESET);
+            NodeList nodes = (NodeList) result;
+            for (int i = 0; i < nodes.getLength(); i++){
+                Node node = nodes.item(i);
+                String concept = node.getChildNodes().item(1).getNodeValue();
+                double relevance = Double.parseDouble(node.getChildNodes().item(2).getNodeValue());
+                conceptsList.add(concept);
+
             }
+            song.setConcepts(conceptsList);
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        song.setConcepts(conceptsList);
 
     }
 
