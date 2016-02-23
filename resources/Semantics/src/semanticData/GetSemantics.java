f989729526af320e2ea08bb55a7f35521fd886e9
Fredrik Heimsæter <freheims@gmail.com>
2016-02-23 13:04:39 +0100

    We got emotions!

--- resources/Semantics/src/semanticData/GetSemantics.java
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -39,4 +39,30 @@ public class GetSemantics {
             e.printStackTrace();
         }
     }
+
+    public static void getEmotions(Song song){
+        if(song.getLyrics() == "") return;
+        ArrayList<String> emotionsList= new ArrayList<>();
+        try {
+            Document doc = alchemyApi.textGetEmotion(song.getLyrics());
+
+            Elements emotions = doc.select("docEmotions");
+
+            double anger = Double.parseDouble(emotions.select("anger").text());
+            if(anger>0.1)emotionsList.add("anger");
+            double disgust = Double.parseDouble(emotions.select("disgust").text());
+            if(disgust>0.1)emotionsList.add("disgust");
+            double fear = Double.parseDouble(emotions.select("fear").text());
+            if(fear>0.1)emotionsList.add("fear");
+            double joy = Double.parseDouble(emotions.select("joy").text());
+            if(joy>0.1)emotionsList.add("joy");
+            double sadness = Double.parseDouble(emotions.select("sadness").text());
+            if(sadness>0.1)emotionsList.add("sadness");
+            song.setEmotions(emotionsList);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
