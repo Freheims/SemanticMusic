a8292a3972a06178e25f01c8c017c21be7d4a4e1
Fredrik Heimsæter <freheims@gmail.com>
2016-02-23 11:59:39 +0100

    It works!!

--- resources/Semantics/src/semanticData/GetSemantics.java
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -6,21 +6,8 @@ import model.Song;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-/*
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import javax.xml.xpath.XPathExpression;
-import java.io.BufferedInputStream;*/
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * Class for getting sematic data from Alchemy
@@ -37,27 +24,16 @@ public class GetSemantics {
         ArrayList<String> conceptsList = new ArrayList<>();
         try {
             Document doc = alchemyApi.textGetRankedConcepts(song.getLyrics());
-            Elements elements = doc.select("concepts");
-            String[] concepts = elements.select("text").text().split(" ");
-            String[] relevances = elements.select("relevance").text().split(" ");
+            Elements elements = doc.select("concepts").select("concept");
 
-            System.out.println();
-            System.out.println("ConLength: " + concepts.length + " " + concepts[0] + " , relLength: " + relevances.length + " " + relevances[0]);
-            System.out.println();
-
-            double[] relevance = new double[relevances.length];
-            for (int i = 0; i< relevances.length;  i++) {
-                String rel = relevances[i];
-                relevance[i] = Double.parseDouble(rel);
-            }
-            int len = Math.min(concepts.length, relevance.length);
-            for (int i = 0; i < len; i++){
-                if (relevance[i]> 0.3){
-                    conceptsList.add(concepts[i]);
-                    System.out.println(concepts[i]);
+            for(int i = 0; i < elements.size(); i++){
+                Element element = elements.get(i);
+                String concept = element.select("text").text();
+                double relevance = Double.parseDouble(element.select("relevance").text());
+                if(relevance > 0.3){
+                    conceptsList.add(concept);
                 }
             }
-
             song.setConcepts(conceptsList);
         }catch (Exception e){
             e.printStackTrace();
