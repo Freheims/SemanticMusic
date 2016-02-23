6835efdcb88f57e3f76553ac2c2895178a384ef9
Fredrik Heimsæter <freheims@gmail.com>
2016-02-23 11:48:12 +0100

    Half working hack

--- resources/Semantics/src/semanticData/GetSemantics.java
+++ resources/Semantics/src/semanticData/GetSemantics.java
@@ -3,6 +3,10 @@ package semanticData;
 import com.alchemyapi.api.AlchemyApi;
 import com.alchemyapi.api.AlchemyApiConfiguration;
 import model.Song;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/*
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -14,7 +18,7 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 import javax.xml.xpath.XPathExpression;
-import java.io.BufferedInputStream;
+import java.io.BufferedInputStream;*/
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -32,13 +36,31 @@ public class GetSemantics {
         if(song.getLyrics() == "") return;
         ArrayList<String> conceptsList = new ArrayList<>();
         try {
-            //TODO
+            Document doc = alchemyApi.textGetRankedConcepts(song.getLyrics());
+            Elements elements = doc.select("concepts");
+            String[] concepts = elements.select("text").text().split(" ");
+            String[] relevances = elements.select("relevance").text().split(" ");
+
+            System.out.println();
+            System.out.println("ConLength: " + concepts.length + " " + concepts[0] + " , relLength: " + relevances.length + " " + relevances[0]);
+            System.out.println();
+
+            double[] relevance = new double[relevances.length];
+            for (int i = 0; i< relevances.length;  i++) {
+                String rel = relevances[i];
+                relevance[i] = Double.parseDouble(rel);
+            }
+            int len = Math.min(concepts.length, relevance.length);
+            for (int i = 0; i < len; i++){
+                if (relevance[i]> 0.3){
+                    conceptsList.add(concepts[i]);
+                    System.out.println(concepts[i]);
+                }
+            }
+
             song.setConcepts(conceptsList);
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
-
-
 }
